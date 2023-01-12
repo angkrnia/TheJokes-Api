2a95b4c6a61c84a0b532bed93f312a7c1c4ab7a3
Angga Kurnia <anggakurnia712@gmail.com>
2023-01-12 10:36:12 +0700

    Create motivasi.js

---
+++ src/motivasi.js
@@ -0,0 +1,34 @@
+const motivasi = [
+  {
+    body: 'Kesuksesan bukanlah tujuan akhir, tetapi merupakan sebuah perjalanan. Jangan berhenti mengejar impian Anda hingga Anda mencapainya',
+  },
+  {
+    body: 'Kegagalan bukanlah akhir dari segalanya, tapi hanya awal dari kesuksesan yang sebenarnya',
+  },
+  {
+    body: 'Tidak ada yang mustahil bagi seseorang yang yakin akan kemampuannya',
+  },
+  {
+    body: 'Mimpi besar hanya dapat dicapai melalui tindakan yang besar pula',
+  },
+  {
+    body: 'Jangan pernah takut untuk gagal, takutlah hanya untuk tidak mencoba',
+  },
+  {
+    body: 'Anda harus memiliki tujuan yang jelas di hidup Anda, atau Anda akan dihanyutkan oleh arus kehidupan',
+  },
+  {
+    body: 'Kesuksesan tidak akan datang mengetuk pintu Anda, Anda harus pergi dan mencari sendiri',
+  },
+  {
+    body: 'Tidak ada yang lebih kuat daripada seseorang yang bertekad untuk mencapai tujuannya',
+  },
+  {
+    body: 'Anda harus berani untuk mengambil risiko, karena itu adalah satu-satunya cara untuk mencapai kesuksesan yang sebenarnya',
+  },
+  {
+    body: 'Kesuksesan bukanlah sesuatu yang diterima, tapi dicapai melalui kerja keras, kesabaran, dan ketekunan',
+  },
+];
+
+module.exports = motivasi;
